e01276ed9b7febf095c89f2556f64cbaf86f8bc3
Muhammad Rivaldy <muhammad.rivaldy@bluebirdgroup.com>
2021-09-09 20:40:41 +0700

    [Config] load config from file & environment variable

--- configuration.go
+++ configuration.go
@@ -3,15 +3,113 @@ package util
 import (
 	"encoding/json"
 	"os"
+	"reflect"
+	"strconv"
 )
 
 // Configuration is a function for get info configuration
 func Configuration(osFile *os.File, model interface{}) error {
+
 	decoder := json.NewDecoder(osFile)
 	err := decoder.Decode(model)
 	if err != nil {
 		return err
 	}
 
+	if err := envConfiguration(model); err != nil {
+		return err
+	}
+
 	return err
+
+}
+
+func envConfiguration(req interface{}) error {
+
+	v := reflect.ValueOf(req)
+	if v.Kind() == reflect.Ptr {
+		v = v.Elem()
+	}
+
+	return setEnv(v.Type(), v)
+
+}
+
+func setEnv(t reflect.Type, v reflect.Value) error {
+
+	if v.Kind() == reflect.Struct {
+		for i := 0; i < t.NumField(); i++ {
+			f := v.Field(i)
+			k := v.Field(i).Kind()
+
+			if k == reflect.Struct {
+				if err := setEnv(f.Type(), f); err != nil {
+					return err
+				}
+
+				continue
+			}
+
+			val := os.Getenv(t.Field(i).Tag.Get("env"))
+			if val == "" {
+				continue
+			}
+
+			switch k {
+			case reflect.String:
+				f.SetString(val)
+			case reflect.Int:
+				valInt, err := strconv.ParseInt(val, 10, 0)
+				if err != nil {
+					return err
+				}
+				f.SetInt(valInt)
+			case reflect.Int8:
+				valInt, err := strconv.ParseInt(val, 10, 8)
+				if err != nil {
+					return err
+				}
+				f.SetInt(valInt)
+			case reflect.Int16:
+				valInt, err := strconv.ParseInt(val, 10, 16)
+				if err != nil {
+					return err
+				}
+				f.SetInt(valInt)
+			case reflect.Int32:
+				valInt, err := strconv.ParseInt(val, 10, 32)
+				if err != nil {
+					return err
+				}
+				f.SetInt(valInt)
+			case reflect.Int64:
+				valInt, err := strconv.ParseInt(val, 10, 64)
+				if err != nil {
+					return err
+				}
+				f.SetInt(valInt)
+			case reflect.Bool:
+				valBool, err := strconv.ParseBool(val)
+				if err != nil {
+					return err
+				}
+				f.SetBool(valBool)
+			case reflect.Float32:
+				valFloat, err := strconv.ParseFloat(val, 32)
+				if err != nil {
+					return err
+				}
+				f.SetFloat(valFloat)
+			case reflect.Float64:
+				valFloat, err := strconv.ParseFloat(val, 64)
+				if err != nil {
+					return err
+				}
+				f.SetFloat(valFloat)
+			}
+		}
+	}
+
+	return nil
+
 }
---
+++ configuration_test.go
@@ -0,0 +1,37 @@
+package util
+
+import (
+	"os"
+	"testing"
+)
+
+type ConfigModel struct {
+	Port        int    `json:"port" env:"PORT"`
+	ServiceName string `json:"service_name" env:"SERVICE_NAME"`
+	Model       struct {
+		ModelName string `json:"model_name" env:"MODEL_NAME"`
+	} `json:"model"`
+}
+
+func init() {
+	os.Setenv("PORT", "8089")
+	os.Setenv("SERVICE_NAME", "your service")
+	os.Setenv("MODEL_NAME", "your model")
+}
+
+// go test -v -run=TestConfiguration
+func TestConfiguration(t *testing.T) {
+	var cfg ConfigModel
+	osFile, err := OpenFile(".", "configuration.json")
+	if err != nil {
+		t.Error(err)
+		return
+	}
+
+	if err := Configuration(osFile, &cfg); err != nil {
+		t.Error(err)
+		return
+	}
+
+	t.Logf("Result: %+v", cfg)
+}
