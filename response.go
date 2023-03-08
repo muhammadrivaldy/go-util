96b6975aa911516ca6def99403f3e0c02a1fb89f
Rivaldy SBS <muhammad.rivaldy@flip.id>
2023-03-08 15:12:01 +0700

    Standardization message of response error

--- response.go
+++ response.go
@@ -1,11 +1,7 @@
 package goutil
 
 import (
-	"strings"
-
 	"github.com/gin-gonic/gin"
-	"golang.org/x/text/cases"
-	"golang.org/x/text/language"
 )
 
 type Response struct {
@@ -18,24 +14,16 @@ type Response struct {
 func ResponseOK(c *gin.Context, code int, obj interface{}) {
 	c.JSON(code, Response{
 		Code:    code,
-		Message: "Success",
+		Message: "success",
 		Data:    obj,
 	})
 }
 
 // ResponseError is a function for send result error to client
 func ResponseError(c *gin.Context, code int, msg error, obj interface{}) {
-
-	message := msg.Error()
-	messages := strings.Split(msg.Error(), " ")
-	if len(messages) > 0 {
-		messages[0] = cases.Title(language.AmericanEnglish).String(messages[0])
-		message = strings.Join(messages, " ")
-	}
-
 	c.JSON(code, Response{
 		Code:    code,
-		Message: message,
+		Message: msg.Error(),
 		Data:    obj,
 	})
 }
