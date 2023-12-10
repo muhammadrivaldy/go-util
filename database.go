a0c4dc361429d6f5e3f0b0e2e1bdd2a0950138f5
Muhammad Rivaldy <muhammadrivaldy16@gmail.com>
2023-12-10 11:58:38 +0700

    Creating function for connect to the postgres

--- database.go
+++ database.go
@@ -10,7 +10,9 @@ import (
 	"time"
 
 	"github.com/jmoiron/sqlx"
+	"gopkg.in/guregu/null.v4"
 	"gorm.io/driver/mysql"
+	"gorm.io/driver/postgres"
 	"gorm.io/gorm"
 	"gorm.io/gorm/logger"
 )
@@ -24,7 +26,7 @@ const (
 	LoggerInfo   = logLevel(logger.Info)
 )
 
-// NewMySQL is a function for set up db connection
+// NewMySQL is a function for set up db mysql connection
 func NewMySQL(user, password, url, schema string, parameters []string) (*sql.DB, error) {
 
 	parameter := strings.Join(parameters, "&")
@@ -35,6 +37,24 @@ func NewMySQL(user, password, url, schema string, parameters []string) (*sql.DB,
 	}
 
 	return db, err
+
+}
+
+// NewPostgreSQL is a function for set up db postgresql connection
+func NewPostgreSQL(user, password, host, schema string, sslMode null.String, port int) (*sql.DB, error) {
+
+	if !sslMode.Valid {
+		sslMode.SetValid("disable")
+	}
+
+	conn := fmt.Sprintf("host=%s port=%d user=%s password=%s dbname=%s sslmode=%s", host, port, user, password, schema, sslMode.String)
+	db, err := sql.Open("postgres", conn)
+	if err != nil {
+		return nil, err
+	}
+
+	return db, err
+
 }
 
 // NewGorm is a override sql from native sql to gorm.
@@ -42,17 +62,22 @@ func NewMySQL(user, password, url, schema string, parameters []string) (*sql.DB,
 // level is a info log of query, value is 1 - 4 (Silent, Error, Warn & Info)
 func NewGorm(db *sql.DB, driverName string, level logLevel) (*gorm.DB, error) {
 
+	gormConfig := &gorm.Config{
+		Logger: logger.New(log.New(os.Stdout, "\r\n", log.LstdFlags), logger.Config{
+			SlowThreshold: time.Second,
+			LogLevel:      logger.LogLevel(level),
+			Colorful:      true,
+		})}
+
 	switch driverName {
 	case "mysql":
-		return gorm.Open(mysql.New(mysql.Config{Conn: db}), &gorm.Config{
-			Logger: logger.New(log.New(os.Stdout, "\r\n", log.LstdFlags), logger.Config{
-				SlowThreshold: time.Second,
-				LogLevel:      logger.LogLevel(level),
-				Colorful:      true,
-			})})
-	default:
-		return nil, errors.New("driver name is not registered")
+		return gorm.Open(mysql.New(mysql.Config{Conn: db}), gormConfig)
+	case "postgres":
+		return gorm.Open(postgres.New(postgres.Config{Conn: db}), gormConfig)
 	}
+
+	return nil, errors.New("driver name is not registered")
+
 }
 
 // NewSqlx is a override sql from native sql to sqlx
--- go.mod
+++ go.mod
@@ -15,13 +15,15 @@ require (
 	github.com/jmoiron/sqlx v1.3.4
 	github.com/mattn/go-sqlite3 v1.14.17
 	github.com/mdp/qrterminal/v3 v3.1.1
-	github.com/stretchr/testify v1.7.0
+	github.com/stretchr/testify v1.8.1
 	go.mau.fi/whatsmeow v0.0.0-20230616194828-be0edabb0bf3
 	go.uber.org/zap v1.18.1
 	gopkg.in/go-playground/validator.v9 v9.31.0
 	gopkg.in/gomail.v2 v2.0.0-20160411212932-81ebce5c23df
+	gopkg.in/guregu/null.v4 v4.0.0
 	gorm.io/driver/mysql v1.1.1
-	gorm.io/gorm v1.21.12
+	gorm.io/driver/postgres v1.5.4
+	gorm.io/gorm v1.25.5
 )
 
 require (
@@ -31,25 +33,30 @@ require (
 	github.com/go-playground/validator/v10 v10.4.1 // indirect
 	github.com/go-sql-driver/mysql v1.6.0 // indirect
 	github.com/gorilla/websocket v1.5.0 // indirect
+	github.com/jackc/pgpassfile v1.0.0 // indirect
+	github.com/jackc/pgservicefile v0.0.0-20221227161230-091c0ba34f0a // indirect
+	github.com/jackc/pgx/v5 v5.4.3 // indirect
 	github.com/jinzhu/inflection v1.0.0 // indirect
-	github.com/jinzhu/now v1.1.2 // indirect
+	github.com/jinzhu/now v1.1.5 // indirect
 	github.com/json-iterator/go v1.1.9 // indirect
 	github.com/leodido/go-urn v1.2.0 // indirect
 	github.com/mattn/go-isatty v0.0.19 // indirect
 	github.com/modern-go/concurrent v0.0.0-20180228061459-e0a39a4cb421 // indirect
 	github.com/modern-go/reflect2 v0.0.0-20180701023420-4b7aa43c6742 // indirect
 	github.com/pmezard/go-difflib v1.0.0 // indirect
+	github.com/rogpeppe/go-internal v1.11.0 // indirect
 	github.com/technoweenie/multipartstreamer v1.0.1 // indirect
 	github.com/ugorji/go/codec v1.1.7 // indirect
 	go.mau.fi/libsignal v0.1.0 // indirect
 	go.uber.org/atomic v1.7.0 // indirect
 	go.uber.org/multierr v1.6.0 // indirect
-	golang.org/x/crypto v0.8.0 // indirect
-	golang.org/x/sys v0.8.0 // indirect
+	golang.org/x/crypto v0.14.0 // indirect
+	golang.org/x/sys v0.13.0 // indirect
+	golang.org/x/text v0.13.0 // indirect
 	google.golang.org/protobuf v1.30.0 // indirect
 	gopkg.in/alexcesaro/quotedprintable.v3 v3.0.0-20150716171945-2caba252f4dc // indirect
 	gopkg.in/go-playground/assert.v1 v1.2.1 // indirect
 	gopkg.in/yaml.v2 v2.2.8 // indirect
-	gopkg.in/yaml.v3 v3.0.0-20210107192922-496545a6307b // indirect
+	gopkg.in/yaml.v3 v3.0.1 // indirect
 	rsc.io/qr v0.2.0 // indirect
 )
